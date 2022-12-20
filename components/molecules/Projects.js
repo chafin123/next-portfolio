ac7aeb8baf539605f279ad8e86e94cd411e38011
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2022-12-20 00:19:17 -0600

    es lint fixs

--- components/molecules/Projects.js
+++ components/molecules/Projects.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import AccordionLayout from "../atoms/AccordionLayout";
 import ProjectShowcase from "../atoms/ProjectShowcase";
 import { useState } from 'react';
